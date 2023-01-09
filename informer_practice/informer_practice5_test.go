57fb13fed31a063b7c1d4ba0a29c8eb929fb01c8
googs1025 <googs1025@gmail.com>
2023-01-09 22:00:35 +0800

    feat: 使用factory初始化clientset配置

--- informer_practice/informer_practice5_test.go
+++ informer_practice/informer_practice5_test.go
@@ -3,10 +3,17 @@ package informer_practice
 import (
 	"fmt"
 	"k8s-informer-controller-practice/src"
-	v1 "k8s.io/api/apps/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	eventv1 "k8s.io/api/events/v1"
+	nodev1 "k8s.io/api/node/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
+	"log"
 	"strings"
 	"testing"
 )
@@ -16,47 +23,73 @@ func newInformerListWatcher(groupVersionResource string, namespace string) cache
 	clientSet := src.InitClient()
 
 	var client rest.Interface
-	if res[0] == "" {
-		client = clientSet.CoreV1().RESTClient()
-	} else {
-		client = clientSet.AppsV1().RESTClient()
-	}
+
 	resource := res[1]
+	// 使用工厂方法初始化客户端
+	client, err := ClientFactory(res[0], clientSet)
+	if err != nil {
+		log.Fatal(err)
+	}
 	selector := fields.Everything()
 	lw := cache.NewListWatchFromClient(client, resource, namespace, selector)
 
 	return lw
 }
 
+// ClientFactory 使用工厂方法扩展，可以自行扩展。
+func ClientFactory(group string, client kubernetes.Interface) (rest.Interface, error) {
+
+	// 可以自己扩展
+	switch group {
+	case corev1.GroupName:
+		return client.CoreV1().RESTClient(), nil
+	case appsv1.GroupName:
+		return client.AppsV1().RESTClient(), nil
+	case batchv1.GroupName:
+		return client.BatchV1().RESTClient(), nil
+	case eventv1.GroupName:
+		return client.EventsV1().RESTClient(), nil
+	case nodev1.GroupName:
+		return client.NodeV1().RESTClient(), nil
+	case rbacv1.GroupName:
+		return client.RbacV1().RESTClient(), nil
+	}
+
+	return nil, fmt.Errorf("no find %s name clientset", group)
+}
 
 func TestObjInformer2(t *testing.T) {
 
-	groupVersionResource := "apps/deployments"
-	lw := newInformerListWatcher(groupVersionResource, "default")
-	obj := &v1.Deployment{}
+	// 这里可以根据需要修改
+	namespace := "default"
+	groupVersionResource := "rbac.authorization.k8s.io/roles"
+
+	lw := newInformerListWatcher(groupVersionResource, namespace)
+	//obj := &appsv1.Deployment{}
+	obj := &rbacv1.Role{}
+	// 注意这里obj与反射的地方都需要修改
 	_, controller := cache.NewInformer(lw, obj, 0, cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
-			deployment, ok := obj.(*v1.Deployment)
+			deployment, ok := obj.(*rbacv1.Role)
 			if !ok {
 				return
 			}
 			fmt.Printf("新增deployment资源: %s\n", deployment.Name)
 		},
 		UpdateFunc: func(oldObj, newObj interface{}) {
-			deployment, ok := oldObj.(*v1.Deployment)
+			deployment, ok := oldObj.(*rbacv1.Role)
 			if !ok {
 				return
 			}
 			fmt.Printf("修改deployment资源: %s\n", deployment.Name)
 		},
 		DeleteFunc: func(obj interface{}) {
-			deployment, ok := obj.(*v1.Deployment)
+			deployment, ok := obj.(*rbacv1.Role)
 			if !ok {
 				return
 			}
 			fmt.Printf("删除deployment资源: %s\n", deployment.Name)
 		},
-		
 	})
 
 	stopCh := make(chan struct{})
